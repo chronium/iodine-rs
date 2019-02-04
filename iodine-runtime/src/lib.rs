2b7e7ace1ea9915fd0f513b6c618ee4a9800cdb4
chronium <chronium@users.noreply.github.com>
2019-02-05 00:47:05 +0200

    Started work on Iodine types

--- iodine-runtime/src/lib.rs
+++ iodine-runtime/src/lib.rs
@@ -1,7 +1,38 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::collections::HashMap;
+
+pub trait IodineObject<'a> {
+    fn get_attributes(&self) -> &'a AttributeDictionary;
+    fn get_attributes_mut(&mut self) -> &'a mut AttributeDictionary;
+}
+
+pub trait IodineType {
+    fn get_name(&self) -> String;
+}
+
+pub type AttributeDictionary<'a> = HashMap<String, Box<&'a dyn IodineObject<'a>>>;
+
+pub enum IodineTypes<'a> {
+    TypeDefinition { attribs: AttributeDictionary<'a> },
+}
+
+impl<'a> IodineType for IodineTypes<'a> {
+    fn get_name(&self) -> String {
+        match self {
+            IodineTypes::TypeDefinition { attribs: _ } => String::from("TypeDef"),
+        }
+    }
+}
+
+impl<'a> IodineObject<'a> for IodineTypes<'a> {
+    fn get_attributes(&self) -> &'a AttributeDictionary {
+        match self {
+            IodineTypes::TypeDefinition { attribs } => attribs,
+        }
+    }
+
+    fn get_attributes_mut(&mut self) -> &'a mut AttributeDictionary {
+        match self {
+            IodineTypes::TypeDefinition { attribs } => attribs,
+        }
     }
 }
